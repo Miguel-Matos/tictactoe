85c53ef604472eb6245307bb53f6fd23b0164237
Miguel Matos <18700730+Miguel-Matos@users.noreply.github.com>
2023-03-29 19:20:04 +0900

    Added more logic

--- src/logic.js
+++ src/logic.js
@@ -16,14 +16,24 @@ const character = (player, symbol) => {
   return {player, symbol};
 };
 
+// Consider converting to factory and making
+// it return one at a time
+// For now this is less code so it stays
 const squares = (() => {
-  const tl = document.querySelector('#tl');
-  const tm = document.querySelector('#tm');
-  const tr = document.querySelector('#tr');
-  const ml = document.querySelector('#ml');
-  const mm = document.querySelector('#mm');
-  const mr = document.querySelector('#mr');
-  const bl = document.querySelector('#bl');
-  const bm = document.querySelector('#bm');
-  const br = document.querySelector('#br');
+  const tl = () => document.querySelector('#tl');
+  const tm = () => document.querySelector('#tm');
+  const tr = () => document.querySelector('#tr');
+  const ml = () => document.querySelector('#ml');
+  const mm = () => document.querySelector('#mm');
+  const mr = () => document.querySelector('#mr');
+  const bl = () => document.querySelector('#bl');
+  const bm = () => document.querySelector('#bm');
+  const br = () => document.querySelector('#br');
+  return {tl, tm, tr, ml, mm, mr, bl, bm, br};
 })();
+
+// Find a way to swap out tl for any other div
+squares.tl().addEventListener('click', (e) => {
+  squares.tl().innerHTML = 'X';
+  squares.tl().disabled = true;
+});
