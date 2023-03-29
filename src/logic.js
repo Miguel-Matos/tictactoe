54139e340dfcd7eb7ed5fd1b1e28fb8ce0467fb0
Miguel Matos <18700730+Miguel-Matos@users.noreply.github.com>
2023-03-29 18:50:16 +0900

    Created module for game squares

--- src/logic.js
+++ src/logic.js
@@ -16,5 +16,14 @@ const character = (player, symbol) => {
   return {player, symbol};
 };
 
-const p1 = character("Player 1", "X");
-const p2 = character("Player 2", "O");
+const squares = (() => {
+  const tl = document.querySelector('#tl');
+  const tm = document.querySelector('#tm');
+  const tr = document.querySelector('#tr');
+  const ml = document.querySelector('#ml');
+  const mm = document.querySelector('#mm');
+  const mr = document.querySelector('#mr');
+  const bl = document.querySelector('#bl');
+  const bm = document.querySelector('#bm');
+  const br = document.querySelector('#br');
+})();
