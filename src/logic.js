f4af57434a15ed2d7e3e98ed130bb3379f911261
Miguel Matos <18700730+Miguel-Matos@users.noreply.github.com>
2023-04-01 22:36:18 +0900

    Restored 2D array

--- src/logic.js
+++ src/logic.js
@@ -1,7 +1,11 @@
 const board = (() => {
   let gameBoard = [];
-  for (let i = 0; i < 9; i++) {
-    gameBoard.push("A");
+  for (let i = 0; i < 3; i++) {
+    let row = [];
+    for (let j = 0; j < 3; j++) {
+      row.push("A");
+    }
+    gameBoard.push(row);
   }
   const test = () => console.log(gameBoard);
   return {test, gameBoard};
@@ -11,7 +15,6 @@ board.test();
 
 // Works fine without
 // Debating on whether to implement or not
-
 // const character = (player, symbol) => {
 //   return {player, symbol};
 // };
@@ -27,19 +30,19 @@ const stateCheck = (() => {
 // Adds X and O to the board
 // Updates text above the board
 const queryAdder = (id) => {
-  const selected = (num) => {
+  const selected = (num1, num2) => {
     document.querySelector(id).addEventListener('click', () => {
       if (document.querySelector(id).innerHTML === "") {
         if (stateCheck.p1Turn) {
           document.querySelector(id).innerHTML = 'X';
           stateCheck.p1Turn = false;
-          board.gameBoard[num] = 'X'
+          board.gameBoard[num1][num2] = 'X'
           console.log(board.gameBoard);
           stateCheck.pText.innerHTML = 'Player 2\'s turn';
         } else {
           document.querySelector(id).innerHTML = 'O';
           stateCheck.p1Turn = true;
-          board.gameBoard[num] = 'O'
+          board.gameBoard[num1][num2] = 'O'
           console.log(board.gameBoard);
           stateCheck.pText.innerHTML = 'Player 1\'s turn';
         };
@@ -51,21 +54,21 @@ const queryAdder = (id) => {
 
 const squareCaller = (() => {
   const tl = queryAdder('#tl');
-  tl.selected(0);
+  tl.selected(0, 0);
   const tm = queryAdder('#tm');
-  tm.selected(1);
+  tm.selected(0, 1);
   const tr = queryAdder('#tr');
-  tr.selected(2);
+  tr.selected(0, 2);
   const ml = queryAdder('#ml');
-  ml.selected(3);
+  ml.selected(1, 0);
   const mm = queryAdder('#mm');
-  mm.selected(4);
+  mm.selected(1, 1);
   const mr = queryAdder('#mr');
-  mr.selected(5);
+  mr.selected(1, 2);
   const bl = queryAdder('#bl');
-  bl.selected(6);
+  bl.selected(2, 0);
   const bm = queryAdder('#bm');
-  bm.selected(7);
+  bm.selected(2, 1);
   const br = queryAdder('#br');
-  br.selected(8);
+  br.selected(2, 2);
 })();
